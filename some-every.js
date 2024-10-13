186a902da9498d169ee50a4c17fb3eb6000d746a
Larry <larryh2612@gmail.com>
2024-10-13 19:36:43 -0400

    Final Version

--- some-every.js
+++ some-every.js
@@ -13,6 +13,8 @@ function hasOddNumber(arr) {
 
 }
 
+//
+
 /*
 Write a function called hasAZero which accepts a number and returns true if that number contains at least one zero. Otherwise, the function should return false
 
